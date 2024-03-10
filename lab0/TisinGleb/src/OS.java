d0015e5439df04ddbb2c32505e0f881611d81733
Tisin-Gleb <tisin1704@gmail.com>
2024-03-11 05:35:11 +0700

    Made OS public abstract due to: Class is exposed outside its defined visibility scope

--- lab0/TisinGleb/src/OS.java
+++ lab0/TisinGleb/src/OS.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 
-abstract class OS{
+public abstract class OS{
     public double getAveragePingTime(){return averagePingTime;}
     public abstract double ping(String s) throws IOException;
     protected double averagePingTime;
